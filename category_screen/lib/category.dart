0780009af348bd66622ef131a02b0b3335de6367
Anton Savinov <Anton.Savinov@Harman.com>
2018-08-31 14:07:21 +0300

    CategoryScreen

---
+++ category_screen/lib/category.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+/// A custom [Category] widget.
+///
+/// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
+/// a colored [InkWell] animation.
+///
+///
+
+class MyCustomWidget extends StatelessWidget {
+  /// Creates a [Category].
+  ///
+  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
+  /// the UI, and the icon that represents it (e.g. a ruler).
+  // TODO: You'll need the name, color, and iconLocation from main.dart
+  final String name;
+  final Color color;
+  final IconData icon;
+
+  const MyCustomWidget(this.name, this.color, this.icon);
+
+  /// Builds a custom widget that shows [Category] information.
+  ///
+  /// This information includes the icon, name, and color for the [Category].
+  @override
+  // This `context` parameter describes the location of this widget in the
+  // widget tree. It can be used for obtaining Theme data from the nearest
+  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
+  // See https://docs.flutter.io/flutter/material/Theme-class.html
+  Widget build(BuildContext context) {
+    // TODO: Build the custom widget here, referring to the Specs.
+    return Material(
+        color: Colors.transparent,
+        child: Container(
+          height: 100.0,
+          child: InkWell(
+              borderRadius: BorderRadius.circular(50.0),
+              highlightColor: color,
+              splashColor: color,
+              onTap: () {
+                print("I was tapped!");
+              },
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Icon(
+                        icon,
+                        size: 60.0,
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        name,
+                        style: TextStyle(fontSize: 24.0),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+        ));
+  }
+}
---
+++ category_screen/lib/category_route.dart
@@ -0,0 +1,79 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+// TODO: Check if we need to import anything
+import 'category.dart';
+
+// TODO: Define any constants
+
+/// Category Route (screen).
+///
+/// This is the 'home' screen of the Unit Converter. It shows a header and
+/// a list of [Categories].
+///
+/// While it is named CategoryRoute, a more apt name would be CategoryScreen,
+/// because it is responsible for the UI at the route's destination.
+class CategoryRoute extends StatelessWidget {
+  const CategoryRoute();
+
+  static const _categoryNames = <String>[
+    'Length',
+    'Area',
+    'Volume',
+    'Mass',
+    'Time',
+    'Digital Storage',
+    'Energy',
+    'Currency',
+  ];
+
+  static const _baseColors = <Color>[
+    Colors.teal,
+    Colors.orange,
+    Colors.pinkAccent,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.greenAccent,
+    Colors.purpleAccent,
+    Colors.red,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Create a list of the eight Categories, using the names and colors
+    // from above. Use a placeholder icon, such as `Icons.cake` for each
+    // Category. We'll add custom icons later.
+
+    // TODO: Create a list view of the Categories
+    final listView = Container(
+      color: Colors.green[100],
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, int index) => MyCustomWidget(
+            _categoryNames[index],
+            _baseColors[index],
+            Icons.alarm_on),
+        itemCount: _categoryNames.length,
+      ),
+    );
+
+    // TODO: Create an App Bar
+    final appBar = AppBar(
+      backgroundColor: Colors.green[100],
+      title: Text(
+        "Unit Converter",
+        style: TextStyle(
+            fontSize: 30.0,
+            color: Colors.black),
+      ),
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      body: listView,
+    );
+  }
+}
